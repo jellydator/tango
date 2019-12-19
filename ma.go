74b3b1a712ce9de08615fb1101e2d95f2e04f4e3
swithek <simon.public@pm.me>
2019-12-19 19:05:37 +0300

    sma

---
+++ ma.go
@@ -0,0 +1,61 @@
+package indc
+
+import (
+	"errors"
+
+	"github.com/shopspring/decimal"
+	"github.com/swithek/chartype"
+)
+
+var (
+	//ErrCandlesCount is returned when insufficient amount of candles is
+	// provided.
+	ErrCandlesCount = errors.New("insufficient amount of candles")
+)
+
+// SMA holds all the neccesary information needed to calculate a simple
+// moving average.
+type SMA struct {
+	// Length specifies how many candles should be used.
+	Length int
+
+	// Offset specifies how many latest candles should be skipped.
+	Offset int
+
+	// Src specifies which price field of the candle should be used.
+	Src chartype.CandleField
+}
+
+// Calc calculates SMA value by using settings stored in the func receiver.
+func (s SMA) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
+	if s.CandlesCount() > len(cc) {
+		return decimal.Zero, ErrCandlesCount
+	}
+
+	res := decimal.Zero
+
+	for i := len(cc) - 1 - s.Offset; i >= len(cc)-s.Length-s.Offset; i++ {
+		res = res.Add(s.Src.Extract(cc[i]))
+	}
+
+	return res.Div(decimal.NewFromInt(int64(s.Length))), nil
+}
+
+// CandlesCount determines the total amount of candles needed for SMA
+// calculation by using settings stored in the receiver.
+func (s SMA) CandlesCount() int {
+	return s.Length + s.Offset
+}
+
+// CalcSMA calculates SMA value by using settings passed as parameters.
+func CalcSMA(cc []chartype.Candle, l, off int, src chartype.CandleField) (decimal.Decimal, error) {
+	s := SMA{Length: l, Offset: off, Src: src}
+	return s.Calc(cc)
+}
+
+// CandlesCountSMA determines the total amount of candles needed for SMA
+// calculation by using settings passed as parameters.
+func CandlesCountSMA(l, off int) int {
+	s := SMA{Length: l, Offset: off}
+	return s.CandlesCount()
+}
