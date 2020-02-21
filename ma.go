2ff96b35f2172fb766828b9c5bd4dbf624d25825
david <dovb@jellydator.dev>
2020-02-21 19:33:04 +0200

    EMA implementation

--- ma.go
+++ ma.go
@@ -13,9 +13,22 @@ var (
 	ErrInvalidCandleCount = errors.New("insufficient amount of candles")
 )
 
-// SMA holds all the neccesary information needed to calculate a simple
+// SMA holds all the neccesary information needed to calculate simple
 // moving average.
 type SMA struct {
+	// Length specifies how many candles should be used.
+	Length int `json:"length"`
+
+	// Offset specifies how many latest candles should be skipped.
+	Offset int `json:"offset"`
+
+	// Src specifies which price field of the candle should be used.
+	Src chartype.CandleField `json:"src"`
+}
+
+// EMA holds all the neccesary information needed to calculate exponential
+// moving average.
+type EMA struct {
 	// Length specifies how many candles should be used.
 	Length int `json:"length"`
 
@@ -58,4 +71,44 @@ func CalcSMA(cc []chartype.Candle, l, off int, src chartype.CandleField) (decima
 func CandleCountSMA(l, off int) int {
 	s := SMA{Length: l, Offset: off}
 	return s.CandleCount()
+}
+
+// Calc calculates EMA value by using settings stored in the func receiver.
+func (e EMA) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
+	if e.CandleCount() > len(cc) {
+		return decimal.Zero, ErrInvalidCandleCount
+	}
+
+	res, err := CalcSMA(cc, e.Length, e.Offset, e.Src)
+
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	mul := decimal.NewFromFloat(2.0 / float64(e.Length+1))
+
+	for i := len(cc) - 1 - e.Offset; i >= len(cc)-e.CandleCount(); i-- {
+		res = e.Src.Extract(cc[i]).Mul(mul).Add(res.Mul(decimal.NewFromInt(1).Sub(mul)))
+	}
+
+	return res, nil
+}
+
+// CandleCount determines the total amount of candles needed for EMA
+// calculation by using settings stored in the receiver.
+func (e EMA) CandleCount() int {
+	return e.Length + e.Offset
+}
+
+// CalcEMA calculates EMA value by using settings passed as parameters.
+func CalcEMA(cc []chartype.Candle, l, off int, src chartype.CandleField) (decimal.Decimal, error) {
+	e := EMA{Length: l, Offset: off, Src: src}
+	return e.Calc(cc)
+}
+
+// CandleCountEMA determines the total amount of candles needed for EMA
+// calculation by using settings passed as parameters.
+func CandleCountEMA(l, off int) int {
+	e := EMA{Length: l, Offset: off}
+	return e.CandleCount()
 }
