db20ef613adc229a9f1066e901d199cd73dab24f
david <dovb@jellydator.dev>
2020-02-29 18:30:30 +0200

    roc template

--- oscillators.go
+++ oscillators.go
@@ -84,3 +84,65 @@ func CandleCountRSI(len, off int) int {
 	r := RSI{Length: len, Offset: off}
 	return r.CandleCount()
 }
+
+// ROC holds all the neccesary information needed to calculate rate
+// of change.
+type ROC struct {
+	// Length specifies how many candles should be used.
+	Length int `json:"length"`
+
+	// Offset specifies how many latest candles should be skipped.
+	Offset int `json:"offset"`
+
+	// Src specifies which price field of the candle should be used.
+	Src chartype.CandleField `json:"src"`
+}
+
+// Validate checks all ROC settings stored in func receiver to make sure that
+// they're meeting each of their own requirements.
+func (r ROC) Validate() error {
+	if r.Length < 1 {
+		return ErrInvalidLength
+	}
+
+	if r.Offset < 0 {
+		return ErrInvalidOffset
+	}
+
+	if err := r.Src.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Calc calculates ROC value by using settings stored in the func receiver.
+func (r ROC) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
+	return decimal.Zero, nil
+}
+
+// CandleCount determines the total amount of candles needed for ROC
+// calculation by using settings stored in the receiver.
+func (r ROC) CandleCount() int {
+	return r.Length + r.Offset
+}
+
+// ValidateROC checks all settings passed as parameters to make sure that
+// they're meeting each of their own requirements.
+func ValidateROC(len, off int, src chartype.CandleField) error {
+	r := ROC{Length: len, Offset: off, Src: src}
+	return r.Validate()
+}
+
+// CalcROC calculates ROC value by using settings passed as parameters.
+func CalcROC(cc []chartype.Candle, len, off int, src chartype.CandleField) (decimal.Decimal, error) {
+	r := ROC{Length: len, Offset: off, Src: src}
+	return r.Calc(cc)
+}
+
+// CandleCountROC determines the total amount of candles needed for ROC
+// calculation by using settings passed as parameters.
+func CandleCountROC(len, off int) int {
+	r := ROC{Length: len, Offset: off}
+	return r.CandleCount()
+}
