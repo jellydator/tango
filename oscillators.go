2531c03fbe66186e8a53870410032e9abb0d0b35
david <dovb@jellydator.dev>
2020-02-29 18:23:34 +0200

    stochastic calculation and tests;

--- oscillators.go
+++ oscillators.go
@@ -84,3 +84,81 @@ func CandleCountRSI(len, off int) int {
 	r := RSI{Length: len, Offset: off}
 	return r.CandleCount()
 }
+
+// STOCH holds all the neccesary information needed to calculate relative
+// strength index.
+type STOCH struct {
+	// Length specifies how many candles should be used.
+	Length int `json:"length"`
+
+	// Offset specifies how many latest candles should be skipped.
+	Offset int `json:"offset"`
+
+	// Src specifies which price field of the candle should be used.
+	Src chartype.CandleField `json:"src"`
+}
+
+// Validate checks all STOCH settings stored in func receiver to make sure that
+// they're meeting each of their own requirements.
+func (s STOCH) Validate() error {
+	if s.Length < 1 {
+		return ErrInvalidLength
+	}
+
+	if s.Offset < 0 {
+		return ErrInvalidOffset
+	}
+
+	if err := s.Src.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Calc calculates STOCH value by using settings stored in the func receiver.
+func (s STOCH) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
+	if s.CandleCount() > len(cc) {
+		return decimal.Zero, ErrInvalidCandleCount
+	}
+
+	l := s.Src.Extract(cc[len(cc)-s.CandleCount()])
+	h := s.Src.Extract(cc[len(cc)-s.CandleCount()])
+
+	for i := len(cc) - s.CandleCount() + 1; i < len(cc)-s.CandleCount()+s.Length; i++ {
+		if s.Src.Extract(cc[i]).LessThan(l) {
+			l = s.Src.Extract(cc[i])
+		}
+		if s.Src.Extract(cc[i]).GreaterThan(h) {
+			h = s.Src.Extract(cc[i])
+		}
+	}
+
+	return (s.Src.Extract(cc[len(cc)-s.Offset-1]).Sub(l)).Div(h.Sub(l)).Mul(decimal.NewFromInt(100)), nil
+}
+
+// CandleCount determines the total amount of candles needed for STOCH
+// calculation by using settings stored in the receiver.
+func (s STOCH) CandleCount() int {
+	return s.Length + s.Offset
+}
+
+// ValidateSTOCH checks all settings passed as parameters to make sure that
+// they're meeting each of their own requirements.
+func ValidateSTOCH(len, off int, src chartype.CandleField) error {
+	s := STOCH{Length: len, Offset: off, Src: src}
+	return s.Validate()
+}
+
+// CalcSTOCH calculates STOCH value by using settings passed as parameters.
+func CalcSTOCH(cc []chartype.Candle, len, off int, src chartype.CandleField) (decimal.Decimal, error) {
+	s := STOCH{Length: len, Offset: off, Src: src}
+	return s.Calc(cc)
+}
+
+// CandleCountSTOCH determines the total amount of candles needed for STOCH
+// calculation by using settings passed as parameters.
+func CandleCountSTOCH(len, off int) int {
+	s := STOCH{Length: len, Offset: off}
+	return s.CandleCount()
+}
