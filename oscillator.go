a604b95833f2ab1c293c011c242284838f618c27
David <SE.David@protonmail.com>
2020-03-01 14:02:05 +0200

    fixed roc

--- oscillator.go
+++ oscillator.go
@@ -2,7 +2,6 @@ package indc
 
 import (
 	"github.com/shopspring/decimal"
-	"github.com/swithek/chartype"
 )
 
 // RSI holds all the neccesary information needed to calculate relative
@@ -75,12 +74,8 @@ func CountRSI(len int) int {
 // STOCH holds all the neccesary information needed to calculate stochastic
 // oscillator.
 type STOCH struct {
-
-	// Length specifies how many candles should be used.
+	// Length specifies how many data points should be used.
 	Length int `json:"length"`
-
-	// Src specifies which price field of the candle should be used.
-	Src chartype.CandleField `json:"src"`
 }
 
 // Validate checks all STOCH settings stored in func receiver to make sure that
@@ -90,68 +85,62 @@ func (s STOCH) Validate() error {
 		return ErrInvalidLength
 	}
 
-	if err := s.Src.Validate(); err != nil {
-		return err
-	}
-
 	return nil
 }
 
 // Calc calculates STOCH value by using settings stored in the func receiver.
-func (s STOCH) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
-	if s.CandleCount() > len(cc) {
-		return decimal.Zero, ErrInvalidCandleCount
+func (s STOCH) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, s.Count())
+	if err != nil {
+		return decimal.Zero, err
 	}
 
-	l := s.Src.Extract(cc[len(cc)-s.CandleCount()])
-	h := s.Src.Extract(cc[len(cc)-s.CandleCount()])
+	l := dd[0]
+	h := dd[0]
 
-	for i := len(cc) - s.CandleCount() + 1; i < len(cc)-s.CandleCount()+s.Length; i++ {
-		if s.Src.Extract(cc[i]).LessThan(l) {
-			l = s.Src.Extract(cc[i])
+	for i := 0; i < len(dd); i++ {
+		if dd[i].LessThan(l) {
+			l = dd[i]
 		}
-		if s.Src.Extract(cc[i]).GreaterThan(h) {
-			h = s.Src.Extract(cc[i])
+		if dd[i].GreaterThan(h) {
+			h = dd[i]
 		}
 	}
 
-	return s.Src.Extract(cc[len(cc)-1]).Sub(l).Div(h.Sub(l)).Mul(decimal.NewFromInt(100)), nil
+	return dd[len(dd)-1].Sub(l).Div(h.Sub(l)).Mul(decimal.NewFromInt(100)), nil
 }
 
-// CandleCount determines the total amount of candles needed for STOCH
+// Count determines the total amount of data points needed for STOCH
 // calculation by using settings stored in the receiver.
-func (s STOCH) CandleCount() int {
+func (s STOCH) Count() int {
 	return s.Length
 }
 
 // ValidateSTOCH checks all settings passed as parameters to make sure that
 // they're meeting each of their own requirements.
-func ValidateSTOCH(len int, src chartype.CandleField) error {
-	s := STOCH{Length: len, Src: src}
+func ValidateSTOCH(len int) error {
+	s := STOCH{Length: len}
 	return s.Validate()
 }
 
 // CalcSTOCH calculates STOCH value by using settings passed as parameters.
-func CalcSTOCH(cc []chartype.Candle, len int, src chartype.CandleField) (decimal.Decimal, error) {
-	s := STOCH{Length: len, Src: src}
-	return s.Calc(cc)
+func CalcSTOCH(dd []decimal.Decimal, len int) (decimal.Decimal, error) {
+	s := STOCH{Length: len}
+	return s.Calc(dd)
 }
 
-// CandleCountSTOCH determines the total amount of candles needed for STOCH
+// CountSTOCH determines the total amount of data points needed for STOCH
 // calculation by using settings passed as parameters.
-func CandleCountSTOCH(len int) int {
+func CountSTOCH(len int) int {
 	s := STOCH{Length: len}
-	return s.CandleCount()
+	return s.Count()
 }
 
 // ROC holds all the neccesary information needed to calculate rate
 // of change.
 type ROC struct {
-	// Length specifies how many candles should be used.
+	// Length specifies how many data points should be used.
 	Length int `json:"length"`
-
-	// Src specifies which price field of the candle should be used.
-	Src chartype.CandleField `json:"src"`
 }
 
 // Validate checks all ROC settings stored in func receiver to make sure that
@@ -161,46 +150,43 @@ func (r ROC) Validate() error {
 		return ErrInvalidLength
 	}
 
-	if err := r.Src.Validate(); err != nil {
-		return err
-	}
-
 	return nil
 }
 
 // Calc calculates ROC value by using settings stored in the func receiver.
-func (r ROC) Calc(cc []chartype.Candle) (decimal.Decimal, error) {
-	if r.CandleCount() > len(cc) {
-		return decimal.Zero, ErrInvalidCandleCount
+func (r ROC) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, r.Count())
+	if err != nil {
+		return decimal.Zero, err
 	}
 
-	l := r.Src.Extract(cc[len(cc)-1])
-	s := r.Src.Extract(cc[len(cc)-r.CandleCount()])
+	l := dd[len(dd)-1]
+	s := dd[0]
 	return l.Sub(s).Div(s).Mul(decimal.NewFromInt(100)).Round(8), nil
 }
 
-// CandleCount determines the total amount of candles needed for ROC
+// Count determines the total amount of data points needed for ROC
 // calculation by using settings stored in the receiver.
-func (r ROC) CandleCount() int {
+func (r ROC) Count() int {
 	return r.Length
 }
 
 // ValidateROC checks all settings passed as parameters to make sure that
 // they're meeting each of their own requirements.
-func ValidateROC(len int, src chartype.CandleField) error {
-	r := ROC{Length: len, Src: src}
+func ValidateROC(len int) error {
+	r := ROC{Length: len}
 	return r.Validate()
 }
 
 // CalcROC calculates ROC value by using settings passed as parameters.
-func CalcROC(cc []chartype.Candle, len int, src chartype.CandleField) (decimal.Decimal, error) {
-	r := ROC{Length: len, Src: src}
-	return r.Calc(cc)
+func CalcROC(dd []decimal.Decimal, len int) (decimal.Decimal, error) {
+	r := ROC{Length: len}
+	return r.Calc(dd)
 }
 
-// CandleCountROC determines the total amount of candles needed for ROC
+// CountROC determines the total amount of data points needed for ROC
 // calculation by using settings passed as parameters.
-func CandleCountROC(len int) int {
+func CountROC(len int) int {
 	r := ROC{Length: len}
-	return r.CandleCount()
+	return r.Count()
 }
