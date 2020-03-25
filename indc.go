63a7d46c8078bc6df1080ee8fa9a51c2fc7ff52d
David <SE.David@protonmail.com>
2020-03-25 20:29:49 +0200

    indices are now in alphabetical order

--- indc.go
+++ indc.go
@@ -2,56 +2,45 @@ package indc
 
 import "github.com/shopspring/decimal"
 
-// MA interface holds all the placeholder functions required that every
-// moving average has to have.
-type MA interface {
-	// Validate makes sure that the moving average is valid.
-	Validate() error
-
-	// Calc calculates moving average value by using settings stored in the func receiver.
-	Calc(dd []decimal.Decimal) (decimal.Decimal, error)
-
-	// Count determines the total amount of data points needed for moving averages
-	// calculation by using settings stored in the receiver.
-	Count() int
+// CCI holds all the neccesary information needed to calculate commodity
+// channel index.
+type CCI struct {
+	// MA configures moving average.
+	MA MA `json:"ma"`
 }
 
-// SMA holds all the neccesary information needed to calculate simple
-// moving average.
-type SMA struct {
-	// Length specifies how many data points should be used.
-	Length int `json:"length"`
-}
-
-// Validate checks all SMA settings stored in func receiver to make sure that
+// Validate checks all CCI settings stored in func receiver to make sure that
 // they're meeting each of their own requirements.
-func (s SMA) Validate() error {
-	if s.Length < 1 {
-		return ErrInvalidLength
+func (c CCI) Validate() error {
+	if c.MA == nil {
+		return ErrMANotSet
+	}
+
+	if err := c.MA.Validate(); err != nil {
+		return err
 	}
 	return nil
 }
 
-// Calc calculates SMA value by using settings stored in the func receiver.
-func (s SMA) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
-	dd, err := resize(dd, s.Count())
+// Calc calculates CCI value by using settings stored in the func receiver.
+func (c CCI) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, c.Count())
 	if err != nil {
 		return decimal.Zero, err
 	}
 
-	res := decimal.Zero
-
-	for i := 0; i < len(dd); i++ {
-		res = res.Add(dd[i])
+	ma, err := c.MA.Calc(dd)
+	if err != nil {
+		return decimal.Zero, err
 	}
 
-	return res.Div(decimal.NewFromInt(int64(s.Length))), nil
+	return dd[len(dd)-1].Sub(ma).Div(decimal.NewFromFloat(0.015).Mul(meanDeviation(dd))).Round(8), nil
 }
 
-// Count determines the total amount of data points needed for SMA
+// Count determines the total amount of data points needed for CCI
 // calculation by using settings stored in the receiver.
-func (s SMA) Count() int {
-	return s.Length
+func (c CCI) Count() int {
+	return c.MA.Count()
 }
 
 // EMA holds all the neccesary information needed to calculate exponential
@@ -104,44 +93,67 @@ func (e EMA) Count() int {
 	return e.Length * 2
 }
 
-// WMA holds all the neccesary information needed to calculate weighted
-// moving average.
-type WMA struct {
-	// Length specifies how many data points should be used.
-	Length int `json:"length"`
+// MACD holds all the neccesary information needed to calculate moving averages
+// convergence divergence.
+type MACD struct {
+	// MA1 configures first moving average.
+	MA1 MA `json:"ma1"`
+
+	// MA2 configures second moving average.
+	MA2 MA `json:"ma2"`
 }
 
-// Validate checks all WMA settings stored in func receiver to make sure that
+// Validate checks all MACD settings stored in func receiver to make sure that
 // they're meeting each of their own requirements.
-func (w WMA) Validate() error {
-	if w.Length < 1 {
-		return ErrInvalidLength
+func (macd MACD) Validate() error {
+	if macd.MA1 == nil || macd.MA2 == nil {
+		return ErrMANotSet
 	}
+
+	if err := macd.MA1.Validate(); err != nil {
+		return err
+	}
+
+	if err := macd.MA2.Validate(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Calc calculates WMA value by using settings stored in the func receiver.
-func (w WMA) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
-	dd, err := resize(dd, w.Count())
+// Calc calculates MACD value by using settings stored in the func receiver.
+func (macd MACD) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, macd.Count())
 	if err != nil {
 		return decimal.Zero, err
 	}
 
-	res := decimal.Zero
-
-	weight := decimal.NewFromFloat(float64(w.Length*(w.Length+1)) / 2.0)
-
-	for i := 0; i < len(dd); i++ {
-		res = res.Add(dd[i].Mul(decimal.NewFromInt(int64(i + 1)).Div(weight)))
+	res1, err := macd.MA1.Calc(dd)
+	if err != nil {
+		return decimal.Zero, err
 	}
+
+	res2, err := macd.MA2.Calc(dd)
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	res := res1.Sub(res2)
 
 	return res, nil
 }
 
-// Count determines the total amount of data points needed for WMA
+// Count determines the total amount of data points needed for MACD
 // calculation by using settings stored in the receiver.
-func (w WMA) Count() int {
-	return w.Length
+func (macd MACD) Count() int {
+	c1 := macd.MA1.Count()
+	c2 := macd.MA2.Count()
+
+	if c1 > c2 {
+		return c1
+	}
+
+	return c2
 }
 
 // ROC holds all the neccesary information needed to calculate rate
@@ -225,6 +237,44 @@ func (r RSI) Count() int {
 	return r.Length
 }
 
+// SMA holds all the neccesary information needed to calculate simple
+// moving average.
+type SMA struct {
+	// Length specifies how many data points should be used.
+	Length int `json:"length"`
+}
+
+// Validate checks all SMA settings stored in func receiver to make sure that
+// they're meeting each of their own requirements.
+func (s SMA) Validate() error {
+	if s.Length < 1 {
+		return ErrInvalidLength
+	}
+	return nil
+}
+
+// Calc calculates SMA value by using settings stored in the func receiver.
+func (s SMA) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, s.Count())
+	if err != nil {
+		return decimal.Zero, err
+	}
+
+	res := decimal.Zero
+
+	for i := 0; i < len(dd); i++ {
+		res = res.Add(dd[i])
+	}
+
+	return res.Div(decimal.NewFromInt(int64(s.Length))), nil
+}
+
+// Count determines the total amount of data points needed for SMA
+// calculation by using settings stored in the receiver.
+func (s SMA) Count() int {
+	return s.Length
+}
+
 // Stoch holds all the neccesary information needed to calculate stochastic
 // oscillator.
 type Stoch struct {
@@ -269,106 +319,56 @@ func (s Stoch) Count() int {
 	return s.Length
 }
 
-// MACD holds all the neccesary information needed to calculate moving averages
-// convergence divergence.
-type MACD struct {
-	// MA1 configures first moving average.
-	MA1 MA `json:"ma1"`
-
-	// MA2 configures second moving average.
-	MA2 MA `json:"ma2"`
+// WMA holds all the neccesary information needed to calculate weighted
+// moving average.
+type WMA struct {
+	// Length specifies how many data points should be used.
+	Length int `json:"length"`
 }
 
-// Validate checks all MACD settings stored in func receiver to make sure that
+// Validate checks all WMA settings stored in func receiver to make sure that
 // they're meeting each of their own requirements.
-func (macd MACD) Validate() error {
-	if macd.MA1 == nil || macd.MA2 == nil {
-		return ErrMANotSet
+func (w WMA) Validate() error {
+	if w.Length < 1 {
+		return ErrInvalidLength
 	}
-
-	if err := macd.MA1.Validate(); err != nil {
-		return err
-	}
-
-	if err := macd.MA2.Validate(); err != nil {
-		return err
-	}
-
 	return nil
 }
 
-// Calc calculates MACD value by using settings stored in the func receiver.
-func (macd MACD) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
-	dd, err := resize(dd, macd.Count())
+// Calc calculates WMA value by using settings stored in the func receiver.
+func (w WMA) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
+	dd, err := resize(dd, w.Count())
 	if err != nil {
 		return decimal.Zero, err
 	}
 
-	res1, err := macd.MA1.Calc(dd)
-	if err != nil {
-		return decimal.Zero, err
-	}
+	res := decimal.Zero
 
-	res2, err := macd.MA2.Calc(dd)
-	if err != nil {
-		return decimal.Zero, err
-	}
+	weight := decimal.NewFromFloat(float64(w.Length*(w.Length+1)) / 2.0)
 
-	res := res1.Sub(res2)
+	for i := 0; i < len(dd); i++ {
+		res = res.Add(dd[i].Mul(decimal.NewFromInt(int64(i + 1)).Div(weight)))
+	}
 
 	return res, nil
 }
 
-// Count determines the total amount of data points needed for MACD
+// Count determines the total amount of data points needed for WMA
 // calculation by using settings stored in the receiver.
-func (macd MACD) Count() int {
-	c1 := macd.MA1.Count()
-	c2 := macd.MA2.Count()
-
-	if c1 > c2 {
-		return c1
-	}
-
-	return c2
+func (w WMA) Count() int {
+	return w.Length
 }
 
-// CCI holds all the neccesary information needed to calculate commodity
-// channel index.
-type CCI struct {
-	// MA configures moving average.
-	MA MA `json:"ma"`
-}
+// MA interface holds all the placeholder functions required that every
+// moving average has to have.
+type MA interface {
+	// Validate makes sure that the moving average is valid.
+	Validate() error
 
-// Validate checks all CCI settings stored in func receiver to make sure that
-// they're meeting each of their own requirements.
-func (c CCI) Validate() error {
-	if c.MA == nil {
-		return ErrMANotSet
-	}
+	// Calc calculates moving average value by using settings stored in the func receiver.
+	Calc(dd []decimal.Decimal) (decimal.Decimal, error)
 
-	if err := c.MA.Validate(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Calc calculates CCI value by using settings stored in the func receiver.
-func (c CCI) Calc(dd []decimal.Decimal) (decimal.Decimal, error) {
-	dd, err := resize(dd, c.Count())
-	if err != nil {
-		return decimal.Zero, err
-	}
-
-	ma, err := c.MA.Calc(dd)
-	if err != nil {
-		return decimal.Zero, err
-	}
-
-	return dd[len(dd)-1].Sub(ma).Div(decimal.NewFromFloat(0.015).Mul(meanDeviation(dd))).Round(8), nil
-}
-
-// Count determines the total amount of data points needed for CCI
-// calculation by using settings stored in the receiver.
-func (c CCI) Count() int {
-	return c.MA.Count()
+	// Count determines the total amount of data points needed for moving averages
+	// calculation by using settings stored in the receiver.
+	Count() int
 }
